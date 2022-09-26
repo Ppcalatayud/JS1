187ae3e1815f261ec5ebae2ac3aa241d57028533
Ppcalatayud <pepe2003.pc@gmail.com>
2022-09-26 23:15:12 +0200

    primer

---
+++ main.js
@@ -0,0 +1,146 @@
+// console.log('Hola')
+// console.error('ErrorConsola')
+// console.warn('esto es warning')
+// alert('holsss')
+// var variable= "Variable"
+// console.log (variable)
+
+
+// alert ('JavaScript funcionando correctamente')
+// 127.0.0.1:5500
+
+
+let variableSinValor
+
+let booleano1 = true
+
+let booleano2 = false
+
+console.log (booleano1);
+
+const PI = 3.14
+const TAU = 2 * PI
+
+console.log (TAU);
+
+const miNombre = "Pepe"
+const miNumeroFav = 19
+
+var booleanos
+
+console.log (miNombre.length);
+    
+console.log (typeof miNumeroFav);
+
+console.log ("Ni nombre es " + miNombre + " y mi número favorito es el "+ miNumeroFav)
+
+let mensaje = "Seré un crack en JavaScript al terminar el bootcamp"
+
+console.log (mensaje.toUpperCase())
+
+let crack = "Hola soy un crack"
+
+console.log (crack.substring (0,5))
+
+
+        let arrayVacio = []
+
+        let arrayNumeros = [0,1,2,3,4,5,6,7,8,9]
+
+        let arrayNumeros2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+        let arrayNumerosPares = [0,2,4,6,8]
+
+        let arrayNumerosNeg = [0,-1,-2,-3,-4,-5,-6,-7,-8,-9]
+
+        let holaMundo = ["Hola", "Mundo"]
+
+        let loGuardoTodo = ["hola", "que", 23, 42.33, "tal"]
+
+        let arrayDeArrays = [[756, "nombre"],[225, "apellido"],[298, "direccion"]]
+
+console.log (loGuardoTodo.length);
+
+console.log(loGuardoTodo[1])
+
+loGuardoTodo.push("Noche");
+
+loGuardoTodo.unshift("Euralio");
+
+console.log (loGuardoTodo)
+
+
+
+const coche = {
+    marca: "Mercedez",
+    modelo: "34",
+    matricula: "8806 KZS"
+}
+
+const casa = {
+    codPostal: 46001,
+    calle: "Pie de la Cruz",
+    portal: 7,
+    piso: "Primer Piso",
+}
+
+const fullStackDeveloper = {
+    arrayLenguajes: ["php", "javascript", "python"],
+    arrayProyectos: ["página personal", "blog"]
+}
+
+const perro = {
+    nombre: "Rufo",
+    raza: "Koker",
+    color: "Marron",
+    edad: 12,
+}
+
+const noticia =  {
+    titular: "Madrid gana la Champions",
+    cuerpo: "15 copas de europa ya"
+}
+
+const persona = {
+    nombre: "Pepe",
+    apellidos: "Calatayud Miquel",
+    edad: 19,
+}
+
+console.log(persona.nombre);
+
+console.log(fullStackDeveloper.arrayLenguajes[1])
+
+const portatil = {
+    marca: "Lenovo",
+}
+
+console.log(portatil.marca)
+
+const concierto = {
+    grupos: ["Loskillaos", "Lorite"]
+}
+
+console.log(concierto.grupos)
+
+const led = {
+    arrayRGB: {
+        rojo: 12,
+        azul: 56,
+        verde: 240,}
+}
+
+console.log(led.arrayRGB)
+
+let o_error = {
+    codigo: "404",
+}
+
+console.log(o_error.codigo)
+
+let grupo = {
+    integrantes: ["Alex", "Jaime", "Pablo"]
+}
+
+console.log(grupo.integrantes[1])
+
